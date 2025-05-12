05f31d090e85d3c4f870953da5adceb0af1627b6
dylana62 <47896460+dylana62@users.noreply.github.com>
2025-05-11 20:52:00 -0400

    Added fade-in and fade-out to scene transitions

--- scripts/door.gd
+++ scripts/door.gd
@@ -1,5 +1,7 @@
 extends Area2D
 
 
-func _on_body_entered(body: Node2D) -> void:
+func _on_body_entered(_body: Node2D) -> void:
+	TransitionScreen.transition()
+	await TransitionScreen.on_transition_finished
 	get_tree().change_scene_to_file("res://scenes/game.tscn")
