2acb9bcaa5a9880b51de16a79928a33c9454d647
dylana62 <47896460+dylana62@users.noreply.github.com>
2025-05-11 21:02:37 -0400

    Added fade out to player death/respawn
    Also changed camera bounds but not important

--- scripts/killzone.gd
+++ scripts/killzone.gd
@@ -10,4 +10,6 @@ func _on_body_entered(body: Node2D) -> void:
 
 
 func _on_timer_timeout() -> void:
+	TransitionScreen.transition()
+	await TransitionScreen.on_transition_finished
 	get_tree().reload_current_scene()
